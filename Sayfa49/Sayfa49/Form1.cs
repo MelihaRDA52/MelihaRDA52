969b45080a90d6c3538ff3398b6d25d3c0430ea6
MelihaRDA52 <91197372+MelihaRDA52@users.noreply.github.com>
2021-09-29 14:54:56 +0300

    Add files via upload

---
+++ Sayfa49/Sayfa49/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sayfa49
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int sayi;
+            sayi = Convert.ToInt32(textBox1.Text);
+            if(sayi % 2 == 0)
+            MessageBox.Show("Bu bir çift sayıdır.");
+
+            else
+
+            MessageBox.Show("Bu bir tek sayıdır.");
+
+        }
+    }
+}
